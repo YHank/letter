70915da44449ad3bea36440c9bfc8d9da77a2045
K Y-Han <ky-han@yhanks-macbookAir.local>
2026-04-08 10:06:28 +0900

    fix: debounce this 바인딩 유지
    - timeout 콜백에서 this 컨텍스트를 유지하도록 수정
    - index.js 파일 로딩 시 캐시 무효화를 위해 버전 번호 갱신

--- js/index.js
+++ js/index.js
@@ -84,7 +84,7 @@ function debounce(func, wait) {
     return function executedFunction(...args) {
         const later = () => {
             clearTimeout(timeout);
-            func(...args);
+            func.apply(this, args);
         };
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
